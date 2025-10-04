bb31cbbe65d42c2d93c85605f4da5794f5bfdcc6
Sreya Mathew <ssmathew@ucdavis.edu>
2025-10-04 13:31:49 -0700

    [Fix] Tailwind: Add global directive to import tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import "./globals.css";
+
 import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
@@ -7,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen">
         <AuthProvider>
           {children}
         </AuthProvider>
