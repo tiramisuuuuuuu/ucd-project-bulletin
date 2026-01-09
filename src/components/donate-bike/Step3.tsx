3ccdfd191043545f73f2ec0730e5a77fcd302bcd
Sreya Mathew <ssmathew@ucdavis.edu>
2026-01-09 14:32:34 -0800

    [Feature] CreatePostSteps: Completed all form components

--- src/components/donate-bike/Step3.tsx
+++ src/components/donate-bike/Step3.tsx
@@ -1,65 +1,63 @@
-import { Form, FormInstance, Input, Select, UploadFile } from "antd";
-import MultipleUpload from "@/components/ui/MultipleUpload";
+import { zUserPost } from "@/types/Posts";
+import { Button, Flex, FormInstance, Result, Spin } from "antd";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import StandardText from "@/components/ui/StandardText";
 
-const dropoffLocationOptions = [
-    {value: 'Silo', label: 'Silo'},
-    {value: 'Memorial Union', label: 'Memorial Union'},
-    {value: 'Shields Library', label: 'Shields Library'}
-]
 
 interface Step3Props {
     form: FormInstance
 }
 
 export default function Step3({ form } : Step3Props) {
-    const [fileList, setFileList] = useState<UploadFile[]>(form.getFieldValue("Dropoff Uploads") ?? [])
-    
-    function updateFileList(fileList: UploadFile[]) {
-        setFileList(fileList);
-        form.setFieldsValue({ "Dropoff Uploads": fileList });
-        form.setFieldsValue({ "Dropoff Image": fileList.map((file: UploadFile) => file.response?.supabase_file_path ?? '') });
-    };
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        async function submitForm() {
+            const post = {
+                title: form.getFieldValue('title'),
+                subtitle: form.getFieldValue('subtitle') ?? '',
+                description: form.getFieldValue('description') ?? '',
+                tags: form.getFieldValue('tags') ?? [],
+                images: form.getFieldValue('images') ?? [],
+                contact_info: form.getFieldValue('contact_info') ?? '',
+            };
+
+            const res = await fetch('/api/v1/posts/create-post', {
+                method: 'POST',
+                body: JSON.stringify(post),
+            });
+
+            const validatedData = zUserPost.safeParse(await res.json());
+            if (!validatedData.success) {
+                console.log(validatedData.error)
+                setError(true);
+            }
+
+            setLoading(false);
+        }
+
+        submitForm();
+    }, []);
 
     return (
         <div className="bg-white text-left px-7 py-10 flex flex-col gap-y-10">
-            <StandardText level={3}>Upload Dropoff Details</StandardText>
-            <Form
-                form={form}
-                variant='filled'
-                style={{ maxWidth: 600, textAlign: 'left' }}
-                initialValues={{ variant: 'filled' }}
-                className="flex flex-col gap-y-10"
-                wrapperCol={{ flex: 1 }}
-            >
-                <Form.Item
-                    label="Dropoff location"
-                    name="Confirm Dropoff Location"
-                    rules={[{ required: true }]}
-                >
-                    <Select options={dropoffLocationOptions} />
-                </Form.Item>
-                
-                <Form.Item label="Dropoff image (please have the bike and recognizable surroundings as the subject)" name="Dropoff Image" rules={[{ required: true }]}>
-                    <MultipleUpload fileList={fileList} updateFileList={updateFileList} />
-                </Form.Item>
-
-                <Form.Item
-                    label="Dropoff Description"
-                    name="Dropoff Description"
-                >
-                    <Input.TextArea placeholder="Describe any details that can make it easier to locate the bike" />
-                </Form.Item>
-
-                <Form.Item
-                    label="Bike lock combination"
-                    name="Bike Lock"
-                    rules={[{ required: true }]}
-                >
-                    <Input />
-                </Form.Item>
-            </Form>
+            {loading ?
+                <Flex vertical>
+                    <Spin size="large" />
+                    <p>Submitting</p>
+                </Flex>
+                : <Result
+                    status={error ? "error" : "success"}
+                    title={error ? "Could not create post" : "Successfully created post!"}
+                    extra={
+                        <Button type="primary" key="console" onClick={() => router.replace('/dashboard')}>
+                            Return to home
+                        </Button>
+                    }
+                />
+            }
         </div>
     )
 }
