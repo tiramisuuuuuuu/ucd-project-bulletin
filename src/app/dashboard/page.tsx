ff9e7f8a982c52f00d0e0e4d53ba0197d9adb387
Sreya Mathew <ssmathew@ucdavis.edu>
2026-01-10 02:54:30 -0800

    [Feature] FloatButton: Add button navigating to "Create Post" page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Tabs } from 'antd';
+"use client";
+
+import { FloatButton, Tabs } from 'antd';
 import type { TabsProps } from 'antd';
 import Feed from '@/components/dashboard/Feed';
 import UserContent from '@/components/dashboard/UserContent';
+import { PlusOutlined } from '@ant-design/icons';
+import { useRouter } from 'next/navigation';
 
 
 const items: TabsProps['items'] = [
@@ -19,10 +22,18 @@ const items: TabsProps['items'] = [
 ];
 
 export default function Page() {
+  const router = useRouter();
 
   return (
     <div className="w-full min-h-full">
       <Tabs defaultActiveKey="1" items={items}  />
+      <FloatButton
+        icon={<PlusOutlined />}
+        description="POST"
+        shape="square"
+        style={{ insetInlineEnd: 24, width: 50, height: 50 }}
+        onClick={() => router.push('/create-post')}
+      />
     </div>
   );
 }
