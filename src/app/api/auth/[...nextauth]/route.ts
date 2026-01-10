e8e629e215cc8c41831b365f70f7ccc15179d191
Sreya Mathew <ssmathew@ucdavis.edu>
2026-01-10 05:22:36 -0800

    [Fix] Linting Error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import { eq } from "drizzle-orm"
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
             clientId: `${process.env.GOOGLE_CLIENT_ID}`,
